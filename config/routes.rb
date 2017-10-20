273ce388195c94fe0339005406ad04fe1a881289
Pratheek <sharma.pratheek59@gmail.com>
2017-10-21 00:35:08 +0530

    Second reallity -OO

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :links
+  get '/users/sign_out' => 'devise/sessions#destroy', as: 'destroy_user_session_path'
   root to: "links#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
